5e9821b6f11a96ccdba662cafeffc10bfa5ae13d
AmirHesam <hesamkangani@gmail.com>
2022-01-01 23:50:55 +0330

    first commit

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <the-header title="Learning Resources"></the-header>
+  <stored-resources :stored-data="this.storedData"></stored-resources>
+</template>
+
+<script>
+import StoredResources from './components/resources-stuff/StoredResources.vue';
+import TheHeader from './components/layouts/TheHeader.vue';
+
+export default {
+  components: {
+    StoredResources,
+    TheHeader,
+  },
+  data() {
+    return {
+      storedData: [
+        {
+          id: '1',
+          title: 'Google',
+          description: 'a search engine',
+          link: 'https://www.google.com',
+        },
+        {
+          id: '2',
+          title: 'W3School',
+          description: 'An online school',
+          link: 'https://www.w3schools.com/',
+        },
+        {
+          id: '3',
+          title: 'JS Book',
+          description: 'a simple book',
+          link: '',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ src/components/resources-stuff/StoredResources.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul>
+    <resource-show
+      v-for="data in storedData"
+      :key="data.id"
+      :id="data.id"
+      :title="data.title"
+      :description="data.description"
+      :link="data.link"
+    ></resource-show>
+  </ul>
+</template>
+
+<script>
+import ResourceShow from './ResourceShow.vue';
+
+export default {
+  components: {
+    ResourceShow,
+  },
+  props: ['stored-data'],
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
